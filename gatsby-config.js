4d396051ecd7f90e845f383a7e3c49217519c27e
zxul767 <willebaldo.gomez@gmail.com>
2020-10-06 12:42:06 -0500

    Add GA tracking ID via environment variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   siteMetadata: {
     title: `Ex Machina`,
@@ -64,7 +68,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-179818876-1`,
+        trackingId: process.env.GA_TRACKING_ID,
       },
     },
     `gatsby-plugin-feed`,
