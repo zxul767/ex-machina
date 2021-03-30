ee18cc4e4ae29faf8d68ed8c47e6c0990760c79e
zxul767 <willebaldo.gomez@gmail.com>
2021-03-29 19:22:01 -0600

    Modify favicon image
    Stop using the generic icon from GatsbyJS

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,13 +80,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Ex-Machina`,
+        short_name: `Ex-Machina`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/ex-machina.png`,
       },
     },
     `gatsby-plugin-offline`,
