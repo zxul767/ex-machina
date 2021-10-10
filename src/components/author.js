c32417d1246e0940a2b84693507fb911d33cdf99
Willebaldo Gómez <willebaldo.gomez@gmail.com>
2021-10-10 02:13:10 -0500

    :bug: Forgot to include `author.js` file

---
+++ src/components/author.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { SocialIcon } from "react-social-icons"
+
+const AuthorBanner = ({ author, social }) => {
+  return (
+    <p className="author">
+      Written by {author}, software engineer and machine learning enthusiast.
+      <div className="social-networks">
+        <SocialIcon
+          className="social-mini-icon"
+          url={`https://twitter.com/${social.twitter}`}
+        />
+        <SocialIcon
+          className="social-mini-icon"
+          url={`https://github.com/${social.github}`}
+        />
+        <SocialIcon
+          className="social-mini-icon"
+          url={`https://linkedin.com/in/${social.linkedin}`}
+        />
+      </div>
+    </p>
+  )
+}
+
+export default AuthorBanner
