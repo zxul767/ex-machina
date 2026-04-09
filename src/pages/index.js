a8ae39fcdd634408bba4e30441c35fc495ecc1b5
Willebaldo Gomez <willebaldo.gomez@gmail.com>
2026-04-08 19:35:25 -0600

    build: migrate Gatsby to v5 and React 18

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,7 +55,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
       edges {
         node {
           excerpt
